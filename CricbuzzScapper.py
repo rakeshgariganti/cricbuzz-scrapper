db0b61d3965bad6699636cbdd73c41a6ec307524
Rakesh Gariganti <rakeshgariganti@gmail.com>
2015-06-21 15:15:35 +0530

    added unity notifications and a bug fix

--- CricbuzzScapper.py
+++ CricbuzzScapper.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import requests
 import time
+import pynotify
 
 URL = "http://synd.cricbuzz.com/j2me/1.0/livematches.xml"
 
@@ -22,47 +23,58 @@ def Main():
 			else:
 				print i.attrib['mchDesc'] +'\n'+ state.attrib['status']+'\n\n'
 
+	print "Live matches:"+str(len(livematches))
+	for x,i in enumerate(livematches):
+		print "\t"+str(x+1)+"."+i
+	print
+	pynotify.init("Start")
 	if len(livematches)>0:
-		print "Live matches:"+str(len(livematches))
-		for x,i in enumerate(livematches):
-			print "\t"+str(x+1)+"."+i
-		print
 		myteam = int(raw_input("Select match:"))
 		while 1:
 			# handle = open('livematches.xml').read()
 			try: 
 				handle = requests.get(URL).content
 			except :
-				print 'Something wrong with your Internet connection'
-				exit(1)
+				print 'Something wrong with your Internet connection,trying again..'
+				pass
 			root = ET.fromstring(handle)
 			for i in root.iter('match'):
 				state = i.find('state')
 				if livematches[myteam-1] == i.attrib['mchDesc']:
 					if state.attrib['mchState']=="inprogress":
-						ee = i.find('mscr')
-						inningsDetail = ee.find('inngsdetail')
-						overs = inningsDetail.attrib['noofovers']
-						crr = inningsDetail.attrib['crr']
-						cprtshp = inningsDetail.attrib['cprtshp']
-						print '\033[91m'+ str(i.attrib['mchDesc']) + '\033[0m'
-						ing = ee.find('btTm')
-						team = ing.attrib['sName']
-						inngs = ing.find('Inngs')
-						score = inngs.attrib['r']
-						overs = inngs.attrib['ovrs']
-						wkts = inngs.attrib['wkts']
-						print str(team)+'\t'+str(score) +"-"+ str(wkts) + "\t" + str(overs)
+						try:
+							ee = i.find('mscr')
+							inningsDetail = ee.find('inngsdetail')
+							overs = inningsDetail.attrib['noofovers']
+							crr = inningsDetail.attrib['crr']
+							cprtshp = inningsDetail.attrib['cprtshp']
+							print '\033[91m'+ str(i.attrib['mchDesc']) + '\033[0m'
+							ing = ee.find('btTm')
+							team = ing.attrib['sName']
+							inngs = ing.find('Inngs')
+							score = inngs.attrib['r']
+							overs = inngs.attrib['ovrs']
+							wkts = inngs.attrib['wkts']
+							note = str(team)+'\t'+str(score) +"-"+ str(wkts) + "\t\n\nOvers: " + str(overs) +"\n\nCRR: "+str(crr) +"\n\nPartnership: "+str(cprtshp)+'\n'
+							pynotify.Notification(summary=note).show()
+							print str(team)+'\t'+str(score) +"-"+ str(wkts) + "\t" + str(overs)
+						except:
+							pass
 
-						ing = ee.find('blgTm')
-						team = ing.attrib['sName']
-						inngs = ing.find('Inngs')
-						score = inngs.attrib['r']
-						overs = inngs.attrib['ovrs']
-						wkts = inngs.attrib['wkts']
-						print str(team)+'\t'+str(score) +"-"+ str(wkts) + "\t" + str(overs)
-						print "\nCRR: "+str(crr) +"\tPartnership: "+str(cprtshp)
-						print state.attrib['status'] +'\n'
+						try:
+							ing = ee.find('blgTm')
+							team = ing.attrib['sName']
+							inngs = ing.find('Inngs')
+							score = inngs.attrib['r']
+							overs = inngs.attrib['ovrs']
+							wkts = inngs.attrib['wkts']
+							note = str(team)+'\t'+str(score) +"-"+ str(wkts) + "\t" + str(overs) +"\nCRR: "+str(crr) +"\tPartnership: "+str(cprtshp)+state.attrib['status'] +'\n'
+							notification = pynotify.Notification(summary=note)
+							notification.show()
+							print str(team)+'\t'+str(score) +"-"+ str(wkts) + "\t" + str(overs)
+							print "\nCRR: "+str(crr) +"\tPartnership: "+str(cprtshp)
+							print state.attrib['status'] +'\n'
+						except:pass
 					else:
 						print '\033[91m'+ str(i.attrib['mchDesc']) + '\033[0m'
 						print '\033[94m'+ state.attrib['status'] + '\033[0m'
